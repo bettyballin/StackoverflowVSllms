b8c2818348579dab00f047cc753db787d23fe592
bettinaballin <bettina.ballin@sit.fraunhofer.de>
2025-01-15 10:57:49 +0100

    cmake

---
+++ build/CMakeFiles/0.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/ssestorage2-data/sse-team/ballin/Stackoverflow/StackoverflowVSllms/cpp_code_files_stackoverflow/0.cpp" "CMakeFiles/0.dir/cpp_code_files_stackoverflow/0.cpp.o" "gcc" "CMakeFiles/0.dir/cpp_code_files_stackoverflow/0.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/0.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "0"
+  "0.pdb"
+  "CMakeFiles/0.dir/cpp_code_files_stackoverflow/0.cpp.o"
+  "CMakeFiles/0.dir/cpp_code_files_stackoverflow/0.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
